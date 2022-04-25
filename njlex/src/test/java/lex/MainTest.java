db5f3d0469f72413a1f9053bf541c0102dc8afba
Nosolution <fudeng125@gmail.com>
2022-04-25 16:22:52 +0800

    REFACTOR: re-organize project structure

---
+++ njlex/src/test/java/lex/MainTest.java
@@ -0,0 +1,11 @@
+package lex;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void main() {
+        Main.main(new String[]{"lex", "F:/Project/NCompiler/test/lextest6.l"});
+    }
+}
